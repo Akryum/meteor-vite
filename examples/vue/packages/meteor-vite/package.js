601ad25c087e7352f660f89baf753108d71f31d8
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-09-01 23:38:54 +0200

    feat(poc): build

--- examples/vue/packages/meteor-vite/package.js
+++ examples/vue/packages/meteor-vite/package.js
@@ -1,18 +1,34 @@
 Package.describe({
-  name: 'meteor-vite',
+  name: 'vite',
   version: '0.1.0',
   summary: 'Integrate the Vite.js bundler with Meteor',
   documentation: 'README.md',
 })
 
-Npm.depends({
+Package.registerBuildPlugin({
+  name: 'meteor-vite',
+  use: [
+    'ecmascript@0.16.2',
+    'caching-compiler@1.2.2',
+  ],
+  sources: [
+    'build.js',
+  ],
+  npmDependencies: {
+    execa: '6.1.0',
+    'fs-extra': '10.1.0',
+    'picocolors': '1.0.0',
+  },
 })
 
 Package.onUse(function(api) {
+  api.use('isobuild:compiler-plugin@1.0.0')
   api.use('ecmascript')
   api.use('webapp')
-  api.mainModule('server.js', 'server')
-  api.addAssets([
-    'worker.mjs'
-  ], 'server')
+  if (process.env.NODE_ENV !== 'production') {
+    api.mainModule('server.js', 'server')
+    api.addAssets([
+      'worker-dev.mjs',
+    ], 'server')
+  }
 })
--- examples/vue/packages/meteor-vite/server.js
+++ examples/vue/packages/meteor-vite/server.js
@@ -3,6 +3,8 @@ import { WebAppInternals } from 'meteor/webapp'
 import { fork } from 'node:child_process'
 
 if (Meteor.isDevelopment) {
+  const cwd = process.env.PWD
+
   const viteSetup = {
     host: 'localhost',
     port: 0,
@@ -22,9 +24,9 @@ if (Meteor.isDevelopment) {
 
   // Use a worker to skip reify and Fibers
   // Use a child process instead of worker to avoid WASM/archived threads error
-  const child = fork(Assets.absoluteFilePath('worker.mjs'), {
+  const child = fork(Assets.absoluteFilePath('worker-dev.mjs'), {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
-    cwd: process.env.PWD,
+    cwd,
     detached: false,
   })
   child.on('message', ({ kind, data }) => {
@@ -32,7 +34,7 @@ if (Meteor.isDevelopment) {
       case 'viteSetup':
         Object.assign(viteSetup, data)
         if (!viteSetup.entryFile) {
-          throw new Meteor.Error(500, 'Missing `meteor.clientEntry` with path to entry file in your vite config.')
+          throw new Meteor.Error(500, 'Missing `meteor.clientEntry` with path to entry file (the one you want to build with Vite) in your vite config.')
         }
       break
       default:
