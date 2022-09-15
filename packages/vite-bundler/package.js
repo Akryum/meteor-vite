bfb3f468d4234f72dfaf9c5d38b0f4f6c1e927d5
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-09-15 11:33:30 +0200

    v0.1.8

--- packages/vite-bundler/package.js
+++ packages/vite-bundler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'vite:bundler',
-  version: '0.1.7',
+  version: '0.1.8',
   summary: 'Integrate the Vite.js bundler with Meteor',
   git: 'https://github.com/Akryum/meteor-vite',
   documentation: 'README.md',
