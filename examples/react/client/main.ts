412d709447e373047473c03f40979414cf50ee37
afrokick <devbyru@gmail.com>
2022-10-13 11:55:58 +0300

    add import react-meteor-data in main file

--- examples/react/client/main.ts
+++ examples/react/client/main.ts
@@ -1,5 +1,8 @@
 import 'meteor/test:lazy'
 import { Cookies } from 'meteor/ostrio:cookies'
+import { useTracker } from "meteor/react-meteor-data";
+
+console.log({useTracker});
 
 const cookies = new Cookies()
 console.log('(meteor) cookies', cookies.get('meteor_login_token'))
