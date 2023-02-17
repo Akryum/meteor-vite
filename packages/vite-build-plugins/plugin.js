561decea637337a118e7d916478e83722f6728e0
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-17 10:03:12 +0100

    Use a class for vite:build-plugins entrypoint.

--- packages/vite-build-plugins/plugin.js
+++ packages/vite-build-plugins/plugin.js
@@ -1,11 +1,19 @@
-const paths = {
-    workerDev: 'worker/worker-dev.mjs',
-    workerProd: 'worker/worker-prod.mjs',
-    meteorStubs: 'worker/vite-plugins/meteor-stubs.mjs',
+ViteBuildPluginBase = class ViteBuildPluginBase {
+    paths = {
+        workerDev: 'worker/worker-dev.mjs',
+        workerProd: 'worker/worker-prod.mjs',
+        meteorStubs: 'worker/vite-plugins/meteor-stubs.mjs',
+    }
+
+    sources;
+
+    constructor() {
+        if (process.env.NODE_ENV === 'production') {
+            this.sources = Object.fromEntries(
+              Object.entries(this.paths).map(([moduleName, relativePath]) => [moduleName, Assets.getText(relativePath)])
+            )
+        }
+    }
 }
-ViteBuildPlugins = {
-    paths,
-    sources: Object.fromEntries(
-      Object.entries(paths).map(([moduleName, relativePath]) => [moduleName, Assets.getText(relativePath)])
-    ),
-}
+
+ViteBuildPlugins = new ViteBuildPluginBase()
