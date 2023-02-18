87b59638f2ef54d79d404a64acdbb7c94f5604b0
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-18 20:45:36 +0100

    Rename logger api module.

--- examples/vue/server/main.ts
+++ examples/vue/server/main.ts
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { LinksCollection } from '/imports/api/links'
-import '/imports/api/logs';
+import '/imports/api/logger';
 import '/imports/api/linksPub'
 
 function insertLink({ title, url }: { title: string, url: string }) {
