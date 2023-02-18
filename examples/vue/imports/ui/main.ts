502ebe4ce7b59ac45006146c3764055c5e6e0754
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-18 19:21:07 +0100

    Update filename for ts-modules tests.

--- examples/vue/imports/ui/main.ts
+++ examples/vue/imports/ui/main.ts
@@ -7,7 +7,7 @@ import { VueMeteor } from './v-meteor'
 import App from './App.vue'
 import { MEOWMEOW } from 'meteor/test:lazy'
 import { check } from 'meteor/check'
-import './tests/modules.test';
+import './tests/ts-modules.test';
 
 console.log('lazy meteor package:', MEOWMEOW)
 console.log(check)
