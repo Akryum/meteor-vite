a3918a82633f093f54e91d60bb53338d199fd9b7
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-19 16:40:20 +0100

    Reduce indentation for meteor-stubs plugin.

--- packages/vite-build-plugins/worker/vite-plugins/meteor-stubs.mjs
+++ packages/vite-build-plugins/worker/vite-plugins/meteor-stubs.mjs
@@ -22,137 +22,139 @@ export function MeteorStubs({ meteorPackagePath, projectJson, isForProduction = 
 }
 
 async function load({ id, meteorPackagePath, projectJson, isForProduction }) {
-    if (id.startsWith('\0meteor/')) {
-        id = id.slice(1);
-        const {
-            mainModule,
-            namedModules,
-            fileContent
-        } = await getSourceText({ id, meteorPackagePath });
-        let code = `const g = typeof window !== 'undefined' ? window : global\n`
-        const moduleList = [mainModule];
-
-        const exportedKeys = []
-
-        // Meteor exports
-        const [, packageName, exported] = /Package\._define\("(.*?)"(?:,\s*exports)?,\s*{\n((?:\s*(?:\w+):\s*\w+,?\n)+)}\)/.exec(fileContent) ?? []
-        if (packageName) {
-            const keys = exported.split('\n').map(line => {
-                const [,key] = /(\w+):\s*(?:\w+)/.exec(line) ?? []
-                return key
-            }).filter(Boolean)
-            exportedKeys.push(...keys)
-            const generated = keys.map(key => `export const ${key} = m.${key}`)
-            code += `const m = g.Package['${packageName}']
-${generated.join('\n')}\n`
-        }
-
-        // Modules re-exports
-        let linkExports = []
-        let linkResult
-        const relativeExportKeys = []
-        const linkReg = /module\d*\.link\("(.*?)", {\n((?:\s*.+:\s*.*\n)+?)\s*}, \d+\);/gm
-        while (linkResult = linkReg.exec(mainModule)) {
-            linkExports.push([linkResult[1], linkResult[2]])
-        }
-        if (linkExports.length) {
-            for (const linkExport of linkExports) {
-                const isRelativeExport = linkExport[0].startsWith('.')
-                let wildcard = ''
-                const generated = linkExport[1].split('\n').map(line => {
-                    const [,source,target] = /\s*"?(.*?)"?:\s*"(.*)"/.exec(line) ?? []
-                    if (source && target) {
-                        if (source === '*' && target === '*') {
-                            wildcard = '*'
-                        }
-                        if (isRelativeExport) {
-                            relativeExportKeys.push(target)
-                        }
-                        if (wildcard || isRelativeExport) {
-                            return;
-                        }
-                        if (source === target) {
-                            return source
-                        }
-                        return `${source} as ${target}`
-                    }
-                }).filter(Boolean)
-                const named = generated.length ? `{${generated.join(', ')}}` : ''
-                if (!isRelativeExport) {
-                    code += `export ${[wildcard, named].filter(Boolean).join(', ')} from '${linkExport[0]}'\n`
-                } else if (wildcard) {
-                    const key = Object.keys(namedModules).find(key => key ?? key.startsWith(`${linkExport[0]}.`))
-                    moduleList.push(namedModules[key]);
-                }
-            }
-        }
-
-        // Module exports
-        let moduleExportsCode = ''
-        let moduleExports = '';
-        let hasModuleDefaultExport = false;
-        for (const content of moduleList) {
-            const [, exports] = /module\d*\.export\({\n((?:.*\n)+?)\s*}\);/.exec(content) ?? []
-            moduleExports += `${exports}\n`;
-            hasModuleDefaultExport = content.match(/module\d*\.exportDefault\(/) || hasModuleDefaultExport;
-
-        }
-        const hasModuleExports = !!moduleExports || !!relativeExportKeys.length
-        if (hasModuleExports || hasModuleDefaultExport) {
-            const sid = stubUid++
-            moduleExportsCode += `${moduleTemplate(id, sid)}\n`
-        }
-        let finalHasModuleExports = false
-        if (hasModuleExports) {
-            const keys = moduleExports.split('\n').map(line => {
-                const [,key] = /(\w+?):\s*/.exec(line) ?? []
-                if (key === 'default') {
-                    hasModuleDefaultExport = true;
-                    return undefined;
-                }
-                return key
-            }).concat(relativeExportKeys).filter(key => key && !exportedKeys.includes(key) && key !== '*')
-            exportedKeys.push(...keys)
-            finalHasModuleExports = keys.length > 0
-            const generated = keys.map(key => `export const ${key} = m2.${key}`)
-            moduleExportsCode += `${generated.join('\n')}\n`
-        }
-        if (hasModuleDefaultExport) {
-            moduleExportsCode += 'export default m2.default ?? m2\n'
-        }
-        if (finalHasModuleExports || hasModuleDefaultExport) {
-            code += moduleExportsCode
-        }
-
-        // Lazy (Isopack)
-        // When bundling for production we can omit this build step.
-        if (isForProduction) {
-            return code;
-        }
-
-        const manifestFile = path.join('.meteor', 'local', 'isopacks', `${id.replace(/^meteor\//, '').replace(/:/g, '_')}`, 'web.browser.json')
-        if (existsSync(manifestFile)) {
-            const manifest = JSON.parse(await fs.readFile(manifestFile, 'utf8'))
-            const resource = manifest.resources.find(r => r.fileOptions.mainModule)
-            if (resource?.fileOptions.lazy) {
-                // Auto-import the package to make it available
-                if (!projectJson.meteor?.mainModule?.client) {
-                    throw new Error(`No meteor.mainModule.client found in package.json`)
-                }
-                const meteorClientEntryFile = path.resolve(process.cwd(), projectJson.meteor.mainModule.client)
-                if (!existsSync(meteorClientEntryFile)) {
-                    throw new Error(`meteor.mainModule.client file not found: ${meteorClientEntryFile}`)
-                }
-                const content = await fs.readFile(meteorClientEntryFile, 'utf8')
-                if (!content.includes(`'${id}'`)) {
-                    await fs.writeFile(meteorClientEntryFile, `import '${id}'\n${content}`)
-                    throw new Error(`Auto-imported package ${id} to ${meteorClientEntryFile}, please reload`)
-                }
-            }
-        }
-
-        return code
+    if (!id.startsWith('\0meteor/')) {
+       return;
     }
+
+    id = id.slice(1);
+    const {
+        mainModule,
+        namedModules,
+        fileContent
+    } = await getSourceText({ id, meteorPackagePath });
+    let code = `const g = typeof window !== 'undefined' ? window : global\n`
+    const moduleList = [mainModule];
+
+    const exportedKeys = []
+
+    // Meteor exports
+    const [, packageName, exported] = /Package\._define\("(.*?)"(?:,\s*exports)?,\s*{\n((?:\s*(?:\w+):\s*\w+,?\n)+)}\)/.exec(fileContent) ?? []
+    if (packageName) {
+        const keys = exported.split('\n').map(line => {
+            const [,key] = /(\w+):\s*(?:\w+)/.exec(line) ?? []
+            return key
+        }).filter(Boolean)
+        exportedKeys.push(...keys)
+        const generated = keys.map(key => `export const ${key} = m.${key}`)
+        code += `const m = g.Package['${packageName}']
+${generated.join('\n')}\n`
+    }
+
+    // Modules re-exports
+    let linkExports = []
+    let linkResult
+    const relativeExportKeys = []
+    const linkReg = /module\d*\.link\("(.*?)", {\n((?:\s*.+:\s*.*\n)+?)\s*}, \d+\);/gm
+    while (linkResult = linkReg.exec(mainModule)) {
+        linkExports.push([linkResult[1], linkResult[2]])
+    }
+    if (linkExports.length) {
+        for (const linkExport of linkExports) {
+            const isRelativeExport = linkExport[0].startsWith('.')
+            let wildcard = ''
+            const generated = linkExport[1].split('\n').map(line => {
+                const [,source,target] = /\s*"?(.*?)"?:\s*"(.*)"/.exec(line) ?? []
+                if (source && target) {
+                    if (source === '*' && target === '*') {
+                        wildcard = '*'
+                    }
+                    if (isRelativeExport) {
+                        relativeExportKeys.push(target)
+                    }
+                    if (wildcard || isRelativeExport) {
+                        return;
+                    }
+                    if (source === target) {
+                        return source
+                    }
+                    return `${source} as ${target}`
+                }
+            }).filter(Boolean)
+            const named = generated.length ? `{${generated.join(', ')}}` : ''
+            if (!isRelativeExport) {
+                code += `export ${[wildcard, named].filter(Boolean).join(', ')} from '${linkExport[0]}'\n`
+            } else if (wildcard) {
+                const key = Object.keys(namedModules).find(key => key ?? key.startsWith(`${linkExport[0]}.`))
+                moduleList.push(namedModules[key]);
+            }
+        }
+    }
+
+    // Module exports
+    let moduleExportsCode = ''
+    let moduleExports = '';
+    let hasModuleDefaultExport = false;
+    for (const content of moduleList) {
+        const [, exports] = /module\d*\.export\({\n((?:.*\n)+?)\s*}\);/.exec(content) ?? []
+        moduleExports += `${exports}\n`;
+        hasModuleDefaultExport = content.match(/module\d*\.exportDefault\(/) || hasModuleDefaultExport;
+
+    }
+    const hasModuleExports = !!moduleExports || !!relativeExportKeys.length
+    if (hasModuleExports || hasModuleDefaultExport) {
+        const sid = stubUid++
+        moduleExportsCode += `${moduleTemplate(id, sid)}\n`
+    }
+    let finalHasModuleExports = false
+    if (hasModuleExports) {
+        const keys = moduleExports.split('\n').map(line => {
+            const [,key] = /(\w+?):\s*/.exec(line) ?? []
+            if (key === 'default') {
+                hasModuleDefaultExport = true;
+                return undefined;
+            }
+            return key
+        }).concat(relativeExportKeys).filter(key => key && !exportedKeys.includes(key) && key !== '*')
+        exportedKeys.push(...keys)
+        finalHasModuleExports = keys.length > 0
+        const generated = keys.map(key => `export const ${key} = m2.${key}`)
+        moduleExportsCode += `${generated.join('\n')}\n`
+    }
+    if (hasModuleDefaultExport) {
+        moduleExportsCode += 'export default m2.default ?? m2\n'
+    }
+    if (finalHasModuleExports || hasModuleDefaultExport) {
+        code += moduleExportsCode
+    }
+
+    // Lazy (Isopack)
+    // When bundling for production we can omit this build step.
+    if (isForProduction) {
+        return code;
+    }
+
+    const manifestFile = path.join('.meteor', 'local', 'isopacks', `${id.replace(/^meteor\//, '').replace(/:/g, '_')}`, 'web.browser.json')
+    if (existsSync(manifestFile)) {
+        const manifest = JSON.parse(await fs.readFile(manifestFile, 'utf8'))
+        const resource = manifest.resources.find(r => r.fileOptions.mainModule)
+        if (resource?.fileOptions.lazy) {
+            // Auto-import the package to make it available
+            if (!projectJson.meteor?.mainModule?.client) {
+                throw new Error(`No meteor.mainModule.client found in package.json`)
+            }
+            const meteorClientEntryFile = path.resolve(process.cwd(), projectJson.meteor.mainModule.client)
+            if (!existsSync(meteorClientEntryFile)) {
+                throw new Error(`meteor.mainModule.client file not found: ${meteorClientEntryFile}`)
+            }
+            const content = await fs.readFile(meteorClientEntryFile, 'utf8')
+            if (!content.includes(`'${id}'`)) {
+                await fs.writeFile(meteorClientEntryFile, `import '${id}'\n${content}`)
+                throw new Error(`Auto-imported package ${id} to ${meteorClientEntryFile}, please reload`)
+            }
+        }
+    }
+
+    return code
 }
 
 function moduleTemplate(id, sid) {
