29c10b68cc345190b14cbc1dd632ea0a78cf0be8
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-02 10:04:59 +0300

    Assert that we can extract the package name.

--- package-parser/test/Parser.test.ts
+++ package-parser/test/Parser.test.ts
@@ -2,14 +2,19 @@ import { describe, expect, it } from 'vitest';
 import { parseModule } from '../src/Parser';
 import { TestTsModulesMock } from './__mocks'
 
-describe('Meteor bundle parser', () => {
+describe('Meteor bundle parser', async () => {
     
-    it('can read the bundle file list', async () => {
-        const parsedModule = await parseModule({
-            fileContent: TestTsModulesMock.fileContent
-        });
-        
+    
+    const parsedModule = await parseModule({
+        fileContent: TestTsModulesMock.fileContent
+    });
+    
+    it('can read the bundle file list', () => {
         expect(parsedModule.fileList).toEqual(TestTsModulesMock.fileList);
+    });
+    
+    it('can detect the package name', () => {
+        expect(parsedModule.packageName).toEqual(TestTsModulesMock.packageName);
     })
     
 })
