cced2f2d423e81dd0d24a82af3649cb32499874d
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-02 07:03:13 +0300

    Correct content type for module mock.

--- package-parser/test/__mocks/index.ts
+++ package-parser/test/__mocks/index.ts
@@ -2,7 +2,7 @@ import FS from 'fs/promises';
 import Path from 'path';
 
 export const TestTsModulesMock = {
-    file: FS.readFile(Path.join(__dirname, 'meteor-bundle/test_ts-modules.js')),
+    file: FS.readFile(Path.join(__dirname, 'meteor-bundle/test_ts-modules.js'), 'utf-8'),
     packageName: 'test:ts-modules',
     exports: {
         // todo
