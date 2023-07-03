7c82e7493b6d2f1d0480f719732e3f34231140dd
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-03 12:18:19 +0300

    Add meteor stub tests.

---
+++ package-parser/test/MeteorStub.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest';
+import { METEOR_STUB_KEY, PACKAGE_SCOPE_KEY, stubTemplate, TEMPLATE_GLOBAL_KEY } from '../src/MeteorStub';
+
+describe('MeteorStub', () => {
+    it('can create default export templates', () => {
+        const template = stubTemplate({
+            stubId: 0,
+            packageId: 'meteor/exampleuser:foobar',
+            moduleExports: [
+                { type: 'export-default' }
+            ],
+            packageScopeExports: {},
+        });
+        
+        expect(template).toContain(`export default ${METEOR_STUB_KEY}.default`)
+    });
+    
+    it('can create package-scope export templates', () => {
+        const template = stubTemplate({
+            stubId: 0,
+            packageId: 'meteor/exampleuser:foobar',
+            packageScopeExports: {
+                'exampleuser:foobar': ['packageScopeExport']
+            },
+            moduleExports: []
+        });
+        
+        expect(template).toContain(`export const packageScopeExport`);
+        expect(template).toContain(`Package['packageScopeExport']`);
+        expect(template).not.toContain('export default');
+    });
+    
+})
