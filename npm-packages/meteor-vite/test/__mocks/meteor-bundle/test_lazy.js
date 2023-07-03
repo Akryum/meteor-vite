f8f5c2b2903a85ef65a97aa53faba97b08ff1172
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-03 21:19:36 +0300

    Add mock for test:lazy

---
+++ npm-packages/meteor-vite/test/__mocks/meteor-bundle/test_lazy.js
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// This is a generated file. You can view the original                  //
+// source in your browser if your browser supports source maps.         //
+// Source maps are supported by all recent versions of Chrome, Safari,  //
+// and Firefox, and by Internet Explorer 11.                            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var global = Package.meteor.global;
+var meteorEnv = Package.meteor.meteorEnv;
+var meteorInstall = Package.modules.meteorInstall;
+var Promise = Package.promise.Promise;
+
+var require = meteorInstall({"node_modules":{"meteor":{"test:lazy":{"index.js":function module(require,exports,module){
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+// packages/test_lazy/index.js                                       //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+                                                                     //
+module.export({
+  MEOWMEOW: () => MEOWMEOW
+});
+const MEOWMEOW = 1;
+///////////////////////////////////////////////////////////////////////
+
+}}}}},{
+  "extensions": [
+    ".js",
+    ".json"
+  ]
+});
+
+
+/* Exports */
+Package._define("test:lazy");
+
+})();
