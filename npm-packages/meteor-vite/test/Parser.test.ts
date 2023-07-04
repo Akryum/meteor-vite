144761de621dbc1b63ef76644f94a25fc5ded116
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-04 16:38:07 +0300

    Refactor parser tests to act more as a quick validation process for all stored mocks.

--- npm-packages/meteor-vite/test/Parser.test.ts
+++ npm-packages/meteor-vite/test/Parser.test.ts
@@ -1,112 +1,62 @@
+import Path from 'path';
 import { describe, expect, it } from 'vitest';
 import { ModuleExport, parseModule } from '../src/Parser';
 import Serialize, { getMainModule } from '../src/util/Serialize';
 import { Check, TsModules } from './__mocks';
 
-describe('Mock package: `test:ts-modules`', async () => {
-    const mockModule = TsModules;
-    const parsedModule = await parseModule({
-        fileContent: TsModules.fileContent
-    });
+describe('Validate known exports for mock packages', () => {
+    const mockPackages = [Check, TsModules];
     
-    it('parsed the package name', () => {
-        expect(parsedModule.packageName).toEqual(mockModule.packageName)
-    });
-    
-    it('detected the correct main module path', () => {
-        expect(parsedModule.mainModulePath).toEqual(mockModule.mainModulePath);
-    });
-    
-    it('has index.ts assigned as the main module export point', () => {
-        const mainModuleExports = getMainModule(parsedModule);
-        expect(mainModuleExports).toEqual(mockModule.modules['index.ts']);
-    })
-    
-    describe('Package files', () => {
-        Object.entries(TsModules.modules).forEach(([filePath, mockExports]: [string, ModuleExport[]]) => {
-            describe(filePath, () => {
-                const parsedExports =  parsedModule.modules[filePath];
-                const namedMockExports = mockExports?.filter(({ type }) => type === 'export')
-                const mockReExports = mockExports?.filter(({ type }) => type === 're-export')
-                
-                
-                it('has parsed exports', () => {
-                    expect(Object.keys(parsedModule.modules)).toContain(filePath);
-                    expect(parsedExports).toBeDefined();
-                });
-                
-                
-                describe('Named exports', () => {
-                    namedMockExports?.forEach((mockExport) => {
-                        it(`export const ${mockExport.name}`, () => {
-                            const expectation = expect.arrayContaining([mockExport])
-                            expect(parsedExports).toEqual(expectation)
-                        })
-                    })
-                })
-                
-                describe('Re-exports', () => {
-                    mockReExports?.forEach((mockExport) => {
-                        it(Serialize.moduleExport(mockExport, mockModule.packageName), () => {
-                            expect(parsedExports).toEqual(
-                                expect.arrayContaining([mockExport])
-                            )
-                        })
-                    })
-                })
+    mockPackages.forEach((mockModule) => {
+        describe(mockModule.packageName, async () => {
+            const parsedModule = await parseModule({ fileContent: mockModule.fileContent });
+            
+            it('parsed the package name', () => {
+                expect(parsedModule.packageName).toEqual(mockModule.packageName)
+            });
+            
+            it('detected the correct main module path', () => {
+                expect(parsedModule.mainModulePath).toEqual(mockModule.mainModulePath);
+            });
+            
+            it('has the correct mainModule exports', () => {
+                const mainModuleExports = getMainModule(parsedModule);
+                const parsedPath = Path.parse(mockModule.mainModulePath);
+                const fileName = parsedPath.base as keyof typeof mockModule['modules'];
+                expect(mainModuleExports).toEqual(mockModule.modules[fileName]);
             })
-        })
-    })
-});
-
-describe('Mock package `check`', async () => {
-    const mockModule = Check;
-    const parsedModule = await parseModule({
-        fileContent: mockModule.fileContent
-    });
-    
-    it('parsed the package name', () => {
-        expect(parsedModule.packageName).toEqual(mockModule.packageName)
-    });
-    
-    it('detected the correct main module path', () => {
-        expect(parsedModule.mainModulePath).toEqual(mockModule.mainModulePath);
-    });
-    
-    it('has index.ts assigned as the main module export point', () => {
-        const mainModuleExports = getMainModule(parsedModule);
-        expect(mainModuleExports).toEqual(mockModule.modules['match.js']);
-    })
-    
-    describe('Package files', () => {
-        Object.entries(mockModule.modules).forEach(([filePath, mockExports]: [string, ModuleExport[]]) => {
-            describe(filePath, () => {
-                const parsedExports =  parsedModule.modules[filePath];
-                const namedMockExports = mockExports?.filter(({ type }) => type === 'export')
-                const mockReExports = mockExports?.filter(({ type }) => type === 're-export')
-                
-                
-                it('has parsed exports', () => {
-                    expect(Object.keys(parsedModule.modules)).toContain(filePath);
-                    expect(parsedExports).toBeDefined();
-                });
-                
-                
-                describe('Named exports', () => {
-                    namedMockExports?.forEach((mockExport) => {
-                        it(`export const ${mockExport.name}`, ({ expect }) => {
-                            const expectation = expect.arrayContaining([mockExport])
-                            expect(parsedExports).toEqual(expectation)
+            
+            describe('Package files', () => {
+                Object.entries(mockModule.modules).forEach(([filePath, mockExports]: [string, ModuleExport[]]) => {
+                    describe(filePath, () => {
+                        const parsedExports =  parsedModule.modules[filePath];
+                        const namedMockExports = mockExports?.filter(({ type }) => type === 'export')
+                        const mockReExports = mockExports?.filter(({ type }) => type === 're-export')
+                        
+                        
+                        it('has parsed exports', () => {
+                            expect(Object.keys(parsedModule.modules)).toContain(filePath);
+                            expect(parsedExports).toBeDefined();
+                        });
+                        
+                        
+                        describe('Named exports', () => {
+                            namedMockExports?.forEach((mockExport) => {
+                                it(`export const ${mockExport.name}`, ({ expect }) => {
+                                    const expectation = expect.arrayContaining([mockExport])
+                                    expect(parsedExports).toEqual(expectation)
+                                })
+                            })
                         })
-                    })
-                })
-                
-                describe('Re-exports', () => {
-                    mockReExports?.forEach((mockExport) => {
-                        it(Serialize.moduleExport(mockExport, mockModule.packageName), ({ expect }) => {
-                            expect(parsedExports).toEqual(
-                                expect.arrayContaining([mockExport])
-                            )
+                        
+                        describe('Re-exports', () => {
+                            mockReExports?.forEach((mockExport) => {
+                                it(Serialize.moduleExport(mockExport, mockModule.packageName), ({ expect }) => {
+                                    expect(parsedExports).toEqual(
+                                        expect.arrayContaining([mockExport])
+                                    )
+                                })
+                            })
                         })
                     })
                 })
