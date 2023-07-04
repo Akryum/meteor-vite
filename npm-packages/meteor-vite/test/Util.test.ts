bc38ed73b3eb9dc1079c2b64c75c3e84919eb5f3
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-04 12:33:58 +0300

    Add assertion for module exports with file extensions.

--- npm-packages/meteor-vite/test/Util.test.ts
+++ npm-packages/meteor-vite/test/Util.test.ts
@@ -1,17 +1,31 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { getModuleExports } from '../src/util/Serialize';
 import { TsModules } from './__mocks';
 
 describe('Utility functions', () => {
     
-    it('can get module exports from a relative path without a file extension', () => {
-        const parserResult = TsModules;
-        const file = getModuleExports({
-            parserResult,
-            importPath: '/explicit-relative-path'
-        })
+    describe('get module exports from path', () => {
+        test('without a file extension', () => {
+            const parserResult = TsModules;
+            const file = getModuleExports({
+                parserResult,
+                importPath: '/explicit-relative-path'
+            })
+            
+            expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
+            expect(file?.fileName).toEqual('explicit-relative-path.ts');
+        });
         
-        expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
-        expect(file?.fileName).toEqual('explicit-relative-path.ts');
+        test('with a file extension', () => {
+            const parserResult = TsModules;
+            const file = getModuleExports({
+                parserResult,
+                importPath: '/explicit-relative-path.ts'
+            })
+            
+            expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
+            expect(file?.fileName).toEqual('explicit-relative-path.ts');
+        })
     })
+    
 })
