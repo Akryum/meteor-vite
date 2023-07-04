896b4bd3b5514881a11cb342d0a78f695a8e27ba
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-04 12:27:56 +0300

    Fix refactored keys in Util tests.

--- npm-packages/meteor-vite/test/Util.test.ts
+++ npm-packages/meteor-vite/test/Util.test.ts
@@ -5,10 +5,13 @@ import { TsModules } from './__mocks';
 describe('Utility functions', () => {
     
     it('can get module exports from a relative path without a file extension', () => {
-        const result = TsModules;
-        const file = getModuleExports({ result, importPath: '/explicit-relative-path' })
+        const parserResult = TsModules;
+        const file = getModuleExports({
+            parserResult,
+            importPath: '/explicit-relative-path'
+        })
         
-        expect(file?.modules).toEqual(TsModules.modules['explicit-relative-path.ts'])
+        expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
         expect(file?.fileName).toEqual('explicit-relative-path.ts');
     })
 })
