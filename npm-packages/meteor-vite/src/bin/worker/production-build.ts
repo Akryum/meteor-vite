484af416bc71fda365aaafa612833611996d428e
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-06 13:17:22 +0300

    Use IPC for production build.

--- npm-packages/meteor-vite/src/bin/worker/production-build.ts
+++ npm-packages/meteor-vite/src/bin/worker/production-build.ts
@@ -4,63 +4,79 @@ import { build, resolveConfig } from 'vite';
 import { MeteorViteConfig } from '../../vite/MeteorViteConfig';
 import { MeteorStubs } from '../../vite';
 import MeteorVitePackage from '../../../package.json';
+import CreateIPCInterface, { IPCReply } from './interface';
 
-export async function StartProductionBuild()  {
-    const [viteOutDir, meteorPackagePath, payloadMarker] = process.argv.slice(2);
-    const viteConfig: MeteorViteConfig = await resolveConfig({}, 'build');
-    
-    if (!viteConfig.meteor?.clientEntry) {
-        throw new Error(`You need to specify an entrypoint in your Vite config! See: ${MeteorVitePackage.homepage}`);
-    }
-    
-    const results = await build({
-        build: {
-            lib: {
-                entry: viteConfig?.meteor?.clientEntry,
-                formats: ['es'],
-            },
-            rollupOptions: {
-                output: {
-                    entryFileNames: 'meteor-entry.js',
-                    chunkFileNames: '[name].js',
-                },
-            },
-            outDir: viteOutDir,
-            minify: false,
-        },
-        plugins: [
-            MeteorStubs({
-                meteorPackagePath,
-                projectJsonContent: JSON.parse(await fs.readFile('package.json', 'utf-8')),
-            }),
-        ],
-    });
-    
-    const result = Array.isArray(results) ? results[0] : results;
-    
-    function validateOutput(rollupResult: typeof result): asserts rollupResult is RollupOutput {
-        if ('output' in results) {
-            return;
+interface BuildOptions {
+    viteOutDir: string;
+    meteorPackagePath: string;
+    payloadMarker: string;
+}
+
+export default CreateIPCInterface({
+    async buildForProduction(
+        reply: IPCReply<{ kind: 'buildResult' | 'payloadMarker', data: string }>,
+        { viteOutDir, meteorPackagePath, payloadMarker }: BuildOptions
+    ) {
+        const viteConfig: MeteorViteConfig = await resolveConfig({}, 'build');
+        
+        if (!viteConfig.meteor?.clientEntry) {
+            throw new Error(`You need to specify an entrypoint in your Vite config! See: ${MeteorVitePackage.homepage}`);
         }
         
-        throw new Error("Error parsing rollup output!")
+        const results = await build({
+            build: {
+                lib: {
+                    entry: viteConfig?.meteor?.clientEntry,
+                    formats: ['es'],
+                },
+                rollupOptions: {
+                    output: {
+                        entryFileNames: 'meteor-entry.js',
+                        chunkFileNames: '[name].js',
+                    },
+                },
+                outDir: viteOutDir,
+                minify: false,
+            },
+            plugins: [
+                MeteorStubs({
+                    meteorPackagePath,
+                    projectJsonContent: JSON.parse(await fs.readFile('package.json', 'utf-8')),
+                }),
+            ],
+        });
+        
+        const result = Array.isArray(results) ? results[0] : results;
+        
+        function validateOutput(rollupResult: typeof result): asserts rollupResult is RollupOutput {
+            if ('output' in results) {
+                return;
+            }
+            
+            throw new Error("Error parsing rollup output!")
+        }
+        
+        validateOutput(result);
+        
+        // Result payload
+        reply({
+            kind: 'payloadMarker',
+            data: payloadMarker,
+        });
+        reply({
+            kind: 'buildResult',
+            data: JSON.stringify({
+                success: true,
+                meteorViteConfig: viteConfig.meteor,
+                output: result.output.map(o => ({
+                    name: o.name,
+                    type: o.type,
+                    fileName: o.fileName,
+                })),
+            })
+        })
     }
-    
-    validateOutput(result);
-    
-    // Result payload
-    process.stdout.write(payloadMarker);
-    process.stdout.write(JSON.stringify({
-        success: true,
-        meteorViteConfig: viteConfig.meteor,
-        output: result.output.map(o => ({
-            name: o.name,
-            type: o.type,
-            fileName: o.fileName,
-        })),
-    }));
-};
-
+})
 
 
 
