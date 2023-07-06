77f6f0e505cc9f5e8a89386e2766c3bf33029e8d
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-06 09:25:17 +0300

    Emit error message if worker entrypoint isn't available.

--- packages/vite-bundler/Workers.ts
+++ packages/vite-bundler/Workers.ts
@@ -1,10 +1,15 @@
 import { fork } from 'node:child_process'
 import Path from 'path';
+import FS from 'fs';
 import type { WorkerMethod, WorkerResponse } from '../../npm-packages/meteor-vite';
 
 // Use a worker to skip reify and Fibers
 // Use a child process instead of worker to avoid WASM/archived threads error
 export function createWorkerFork(hooks: WorkerHooks) {
+    if (!FS.existsSync(workerPath)) {
+        throw new Error(`Worker entrypoint doesn't exist! You may need to run "$ npm run build" in the '/npm-packages/meteor-vite' directory`)
+    }
+    
     const child = fork(workerPath, ['--enable-source-maps'], {
         stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
         cwd,
