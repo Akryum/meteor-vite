9fdec57aaee34dc40a8a42d1b35f4cc54695f097
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-09 16:21:09 +0300

    Refactor PackageSubmodule with new pseudo-implementation for module management.

--- npm-packages/meteor-vite/src/meteor/package/PackageSubmodule.ts
+++ npm-packages/meteor-vite/src/meteor/package/PackageSubmodule.ts
@@ -1,15 +1,7 @@
-import { FileRequestData } from '../../vite/ViteLoadRequest';
-import MeteorPackage from './MeteorPackage';
-import { ModuleExport, PackageScopeExports } from './Parser';
+import type MeteorPackage from './MeteorPackage';
+import type { ModuleExport } from './Parser';
 
 export class PackageSubmodule {
-    
-    /**
-     * Full import path for the package's requested module.
-     * @example
-     * 'ostrio:cookies/cookie-store.js'
-     */
-    public readonly importPath: string;
     
     /**
      * Relative path from the package name to the module containing these exports.
@@ -26,25 +18,46 @@ export class PackageSubmodule {
     public readonly exports: ModuleExport[];
     
     /**
-     * {@link FileRequestData}
-     */
-    public readonly packageId: FileRequestData['packageId'];
-    
-    /**
      * The Meteor package this submodule belongs to.
      * @type {MeteorPackage}
      */
     public readonly meteorPackage: MeteorPackage;
     
-    constructor({ meteorPackage, importPath, packageId }: SubmoduleOptions) {
-        this.meteorPackage = meteorPackage;
-        this.importPath = importPath;
-        this.packageId = packageId;
-        
-        const { modulePath, exports } = this.meteorPackage.getExports({ importPath });
+    /**
+     * Full import path for the package's requested module.
+     * @example
+     * 'meteor/ostrio:cookies/cookie-store.js'
+     */
+    public get fullImportPath() {
+        return `${this.meteorPackage.packageId}${this.modulePath ? `/${this.modulePath}` : ''}`
+    };
+    
+    constructor({ meteorPackage, modulePath, exports }: PackageSubmoduleOptions) {
         this.exports = exports;
         this.modulePath = modulePath;
+        this.meteorPackage = meteorPackage;
     }
 }
 
-type SubmoduleOptions = { meteorPackage: MeteorPackage } & Pick<PackageSubmodule, 'importPath' | 'packageId'>
+interface PackageSubmoduleOptions {
+    /**
+     * ESM exports from the Meteor package module.
+     * @example
+     * export const foo = '...'
+     */
+    exports: ModuleExport[];
+    
+    /**
+     * Path relative to the current package to the module containing the these exports.
+     * @example
+     * 'meteor/ddp' // -> ''
+     * 'meteor/ostrio:cookies' // -> ''
+     * 'meteor/ostrio:cookies/cookie-store.js' // -> 'cookie-store.js'
+     */
+    modulePath: string;
+    
+    /**
+     * Instance of the Meteor package this submodule belongs to.
+     */
+    meteorPackage: MeteorPackage;
+}
