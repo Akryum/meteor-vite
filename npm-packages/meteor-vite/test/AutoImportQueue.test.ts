96e14e54791380822ff3573d0378badb7306b0d8
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-09 22:04:04 +0300

    Refactor auto import with existing content assertion

--- npm-packages/meteor-vite/test/AutoImportQueue.test.ts
+++ npm-packages/meteor-vite/test/AutoImportQueue.test.ts
@@ -2,21 +2,18 @@ import FS from 'fs/promises';
 import { describe, expect, it } from 'vitest';
 import AutoImportQueue from '../src/meteor/package/AutoImportQueue';
 import { viteAutoImportBlock } from '../src/meteor/package/StubTemplate';
-import { RefreshNeeded } from '../src/vite/ViteLoadRequest';
-import { AutoImportMock, OstrioCookies, TestLazy, TsModules } from './__mocks';
+import { AutoImportMock } from './__mocks';
 
 describe('Package auto-imports', async () => {
     it('can add auto-imports to a module with existing content', async () => {
         const requestId = 'meteor/test:auto-imports';
         const { filePath: targetFile } = await AutoImportMock.useEntrypoint('withUnrelatedImports');
-        await AutoImportQueue.write({ requestId, targetFile, write(content) {
-            return viteAutoImportBlock({ id: requestId, content });
-        }}).catch((error) => {
-            if (error instanceof RefreshNeeded) {
-                return;
-            }
-            throw error;
-        });
+        await AutoImportQueue.write({
+            requestId,
+            targetFile,
+            skipRestart: true,
+            write: (content) => viteAutoImportBlock({ id: requestId, content }),
+        })
         const newContent = await FS.readFile(targetFile, 'utf-8');
         
         expect(newContent).toContain(`'${requestId}'`);
