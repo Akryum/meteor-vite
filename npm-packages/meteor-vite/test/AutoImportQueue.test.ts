313b64ff0c254aba17ef7cce36ccfb2afc53eb0d
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-09 22:38:57 +0300

    Assert that AutoImportQueue writes lines one by one during concurrent import requests.

--- npm-packages/meteor-vite/test/AutoImportQueue.test.ts
+++ npm-packages/meteor-vite/test/AutoImportQueue.test.ts
@@ -24,6 +24,7 @@ describe('Package auto-imports', async () => {
         it('does not modify original content', async () => {
             const { meteorEntrypoint, template } = await AutoImportMock.useEntrypoint('withUnrelatedImports');
             const importString = 'meteor/test:auto-imports';
+            
             await AutoImportQueue.write({
                 importString,
                 meteorEntrypoint,
@@ -33,6 +34,41 @@ describe('Package auto-imports', async () => {
             
             expect(newContent).toContain(`'${importString}'`);
             expect(newContent).toContain(template);
+        });
+        
+        it('adds import lines one by one during concurrent import requests', async () => {
+            type AutoImportResult = { index: number, lastResolvedIndex: number, importString: string };
+            let lastResolvedIndex = -1;
+            const MOCK_IMPORT_COUNT = 25;
+            const pendingImports: Promise<AutoImportResult>[] = [];
+            const { meteorEntrypoint, template } = await AutoImportMock.useEntrypoint('withUnrelatedImports');
+            
+            for (let index = 0; index < MOCK_IMPORT_COUNT; index++) {
+                const importString = `meteor/test:auto-imports-${index}`
+                pendingImports.push(AutoImportQueue.write({
+                    importString,
+                    meteorEntrypoint,
+                    skipRestart: true,
+                }).then(() => {
+                    const result: AutoImportResult = {
+                        importString,
+                        lastResolvedIndex,
+                        index,
+                    }
+                    lastResolvedIndex = index;
+                    return result;
+                }));
+            }
+            
+            const imports = await Promise.all(pendingImports);
+            const newContent = FS.readFile(meteorEntrypoint, 'utf-8');
+            
+            expect(lastResolvedIndex).toEqual(MOCK_IMPORT_COUNT - 1);
+            
+            imports.forEach((result) => {
+                expect(result.lastResolvedIndex).toEqual(result.index - 1);
+                expect(newContent).toContain(`'${result.importString}'`);
+            })
         })
     })
 })
