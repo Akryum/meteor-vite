4ea099a413b8f5ed9816c2d9664c31594bb70896
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-09 12:23:43 +0300

    Refactor utility tests to use MeteorPackage class.

---
+++ npm-packages/meteor-vite/test/MeteorPackage.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from 'vitest';
+import { TsModules } from './__mocks';
+
+describe('MeteorPackage', () => {
+    describe('Utility methods', () => {
+        describe('get module exports from path', () => {
+            
+            test('without a file extension', () => {
+                const file = TsModules.meteorPackage.getExports({
+                    importPath: '/explicit-relative-path'
+                });
+                
+                expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
+                expect(file?.modulePath).toEqual('explicit-relative-path.ts');
+            });
+            
+            test('with a file extension', () => {
+                const file = TsModules.meteorPackage.getExports({
+                    importPath: '/explicit-relative-path.ts'
+                });
+                
+                expect(file?.exports).toEqual(TsModules.modules['explicit-relative-path.ts'])
+                expect(file?.modulePath).toEqual('explicit-relative-path.ts');
+            })
+            
+        })
+    })
+})
