4b9a016afa5b5d36c78f2c73e303eef89a08f731
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-10 07:47:03 +0300

    Require environment variable for debug logs

--- npm-packages/meteor-vite/src/Logger.ts
+++ npm-packages/meteor-vite/src/Logger.ts
@@ -2,7 +2,7 @@ export default {
     info: (...params: Parameters<typeof console.log>) => console.log(...formatMessage(params)),
     warn: (...params: Parameters<typeof console.log>) => console.warn(...formatMessage(params)),
     error: (...params: Parameters<typeof console.log>) => console.error(...formatMessage(params)),
-    debug: (...params: Parameters<typeof console.log>) => console.debug(...formatMessage(params)),
+    debug: (...params: Parameters<typeof console.log>) => process.env.ENABLE_DEBUG_LOGS && console.debug(...formatMessage(params)),
 }
 
 function formatMessage([message, ...params]: Parameters<typeof console.log>): Parameters<typeof console.log> {
