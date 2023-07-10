9197602c1c56174a4557e7f0f89b6c7aed150b79
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-10 21:15:49 +0300

    Add utility binary for checking if Meteor package has changesets.

---
+++ .bin/release-meteor-package.mjs
@@ -0,0 +1,29 @@
+import { execSync } from 'child_process';
+import Path from 'path';
+import FS from 'fs/promises';
+
+const meteorPackage = {
+    releaseName: 'vite-bundler',
+    packageJsPath: Path.join(__dirname, '../packages/vite-bundler/package.js'),
+}
+
+const PACKAGE_VERSION_REGEX = /version:\s*'(?<version>[\d.]+)'\s*,/;
+
+execSync('changeset status --output changeset-status.json');
+const status = import('../changeset-status.json').then(async ({ releases }) => {
+    const release = releases.find(({ name }) => meteorPackage.releaseName);
+
+    if (!release) {
+        console.log('No pending releases found for %s', meteorPackage.releaseName);
+        return;
+    }
+
+    console.log(`New version ${release.newVersion} for ${meteorPackage.releaseName} detected`);
+    let packageJsContent = await FS.readFile(meteorPackage.packageJsPath, 'utf-8');
+    const currentVersion = packageJsContent.match(PACKAGE_VERSION_REGEX)?.groups?.version
+    packageJsContent = packageJsContent.replace(PACKAGE_VERSION_REGEX, `version: '${release.newVersion}'`);
+    await FS.write(meteorPackage.packageJsPath, packageJsContent);
+    console.log(`Changed version in package.js from v${currentVersion} to v${release.newVersion}`);
+    execSync(`git add ${meteorPackage.packageJsPath}`);
+    execSync(`git commit -m 'Bump ${meteorPackage.releaseName} version to ${release.newVersion}'`);
+})
