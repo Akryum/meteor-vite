8f93d93cc9a9197f1422832e087490341ebd3586
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-10 21:31:49 +0300

    Add error handler and improve log messages in release-meteor-package.mjs

--- .bin/release-meteor-package.mjs
+++ .bin/release-meteor-package.mjs
@@ -25,18 +25,21 @@ changesetStatus.then(async ({ releases }) => {
     const release = releases.find(({ name }) => meteorPackage.releaseName);
 
     if (!release) {
-        console.log('No pending releases found for %s', meteorPackage.releaseName);
+        console.log('⚠️  No pending releases found for %s', meteorPackage.releaseName);
         return;
     }
 
-    console.log(`New version ${release.newVersion} for ${meteorPackage.releaseName} detected`);
+    console.log(`ℹ️  New version ${release.newVersion} for ${meteorPackage.releaseName} detected`);
 
     let packageJsContent = await FS.readFile(meteorPackage.packageJsPath, 'utf-8');
     const currentVersion = packageJsContent.match(PACKAGE_VERSION_REGEX)?.groups?.version
+    if (!currentVersion) {
+        throw new Error(`Unable to read version from ${meteorPackage.releaseName} package.js`)
+    }
     packageJsContent = packageJsContent.replace(PACKAGE_VERSION_REGEX, `version: '${release.newVersion}',`);
     await FS.writeFile(meteorPackage.packageJsPath, packageJsContent);
 
-    console.log(`Changed version in package.js from v${currentVersion} to v${release.newVersion}`);
+    console.log(`✅  Changed version in package.js from v${currentVersion} to v${release.newVersion}`);
 
     shell(`git add ${meteorPackage.packageJsPath}`);
     shell(`git commit -m 'Bump ${meteorPackage.releaseName} version to ${release.newVersion}'`);
