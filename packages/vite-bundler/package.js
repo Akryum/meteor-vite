d4baa3a620c5281fd1973bfff79b230330a55f7d
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-10 22:00:34 +0300

    Update vite-bundler to v1.0.1

--- packages/vite-bundler/package.js
+++ packages/vite-bundler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jorgenvatle:vite-bundler',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Integrate the Vite.js bundler with Meteor',
   git: 'https://github.com/JorgenVatle/meteor-vite',
   documentation: 'README.md',
