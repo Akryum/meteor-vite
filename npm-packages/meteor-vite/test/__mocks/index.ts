9f53a0f5ff818d4c8aa32ea755565c7e1369b145
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-13 15:48:34 +0300

    Add entry to mock index for rdb:svelte-meteor-data

--- npm-packages/meteor-vite/test/__mocks/index.ts
+++ npm-packages/meteor-vite/test/__mocks/index.ts
@@ -104,6 +104,21 @@ export const OstrioCookies = prepareMock({
     mainModulePath: '/node_modules/meteor/ostrio:cookies/cookies.js',
 })
 
+export const RdbSvelteMeteorData = prepareMock({
+    packageName: 'rdb:svelte-meteor-data',
+    fileName: 'rdb_svelte-meteor-data.js',
+    modules: {
+        'use-session.js': [
+            { type: 'export-default', }
+        ],
+        'use-tracker.js': [
+            { type: 'export-default', }
+        ],
+    },
+    packageScopeExports: {},
+    mainModulePath: '/node_modules/meteor/rdb:svelte-meteor-data/index.js',
+})
+
 function prepareMock<Modules extends ModuleList>({ fileName, ...details }: PrepareMockModule<Modules>): MockModule<Modules> {
     const filePath = Path.join(__dirname, `meteor-bundle/${fileName}`);
     const packageId = `meteor/${details.packageName}`;
