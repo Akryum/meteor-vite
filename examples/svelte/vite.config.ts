3976f007f55a0b467911eaa398693b4126435517
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-13 13:36:57 +0300

    Add Vite config to Svelte example

---
+++ examples/svelte/vite.config.ts
@@ -0,0 +1,20 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { MeteorViteConfig } from '../../npm-packages/meteor-vite';
+
+declare module 'vite' {
+  interface UserConfig extends Pick<MeteorViteConfig, 'meteor'> {}
+}
+
+defineConfig({
+  plugins: [
+    svelte(),
+  ],
+
+  meteor: {
+    clientEntry: 'imports/ui/main.ts',
+    stubValidation: {
+      warnOnly: true,
+    }
+  },
+})
