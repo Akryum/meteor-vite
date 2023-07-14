7058821c1b49cab4b2ddcf49aa576933a2f1818c
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-15 02:03:15 +0300

    Add ExportEntry class for more object-oriented export serialization.

--- npm-packages/meteor-vite/src/meteor/package/PackageSubmodule.ts
+++ npm-packages/meteor-vite/src/meteor/package/PackageSubmodule.ts
@@ -1,5 +1,6 @@
 import type MeteorPackage from './MeteorPackage';
 import type { ModuleExport } from './Parser';
+import { METEOR_STUB_KEY } from './StubTemplate';
 
 export class PackageSubmodule {
     
@@ -36,6 +37,49 @@ export class PackageSubmodule {
         this.exports = exports;
         this.modulePath = modulePath;
         this.meteorPackage = meteorPackage;
+    }
+}
+
+class ExportEntry implements ModuleExport {
+    public readonly parentModule: PackageSubmodule;
+    public readonly from;
+    public readonly as;
+    public readonly type;
+    public readonly name;
+    public readonly id;
+    
+    constructor(details: { entry: ModuleExport, parentModule: PackageSubmodule }) {
+        this.parentModule = details.parentModule;
+        const { from, as, type, name, id } = details.entry;
+        this.from = from;
+        this.as = as;
+        this.type = type;
+        this.name = name;
+        this.id = id;
+    }
+    
+    public serialize() {
+        if (this.type === 're-export') {
+            let from = this.from?.startsWith('.')
+                       ? `${this.parentModule.meteorPackage.packageId}/${this.from?.replace(/^[./]+/, '')}`
+                       : this.from;
+            
+            if (this.name?.trim() === '*' && !this.as) {
+                return `export * from '${from}';`
+            }
+            
+            return `export { ${this.name} ${this.as && `as ${this.as} ` || ''}} from '${from}';`
+        }
+        
+        if (this.type === 'export-default' || (this.type === 'export' && this.name === 'default')) {
+            return `export default ${METEOR_STUB_KEY}.default ?? ${METEOR_STUB_KEY};`
+        }
+        
+        if (this.type === 'export') {
+            return `export const ${this.name} = ${METEOR_STUB_KEY}.${this.name};`
+        }
+        
+        throw new Error('Tried to format an non-supported module export!');
     }
 }
 
