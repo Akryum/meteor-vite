594dfc063fef7916cac07eb5c901878819e08172
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-14 20:27:47 +0300

    Add missing optional operator to DeepPartial utility.

--- npm-packages/meteor-vite/src/HelperTypes.ts
+++ npm-packages/meteor-vite/src/HelperTypes.ts
@@ -1,3 +1,3 @@
 export type DeepPartial<TObject> = TObject extends {} ? {
-    [key in keyof TObject]: DeepPartial<TObject[key]>
+    [key in keyof TObject]?: DeepPartial<TObject[key]>
 } : TObject;
