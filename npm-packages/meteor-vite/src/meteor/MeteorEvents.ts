d4fb64cceb331f007074f81e0971f51d9b58ede1
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-14 23:50:43 +0300

    Add MeteorEvents class for handling meteor's IPC messages.

---
+++ npm-packages/meteor-vite/src/meteor/MeteorEvents.ts
@@ -0,0 +1,26 @@
+import { EventEmitter } from 'events';
+
+type MeteorIPCTopic = 'webapp-reload-client' | 'webapp-pause-client' | 'client-refresh';
+
+export type MeteorIPCMessage = {
+    type: 'METEOR_IPC_MESSAGE',
+    responseId: string,
+    topic: MeteorIPCTopic,
+    encodedPayload: string
+}
+
+export default new class MeteorEvents extends EventEmitter {
+    public awaitClientRefresh() {
+        return new Promise((resolve, reject) => {
+            this.once('webapp-reload-client', resolve);
+        })
+    }
+    
+    public transmit(event: MeteorIPCTopic) {
+        super.emit(event);
+    }
+    
+    protected waitFor(eventName: MeteorIPCTopic, listener: (...args: any[]) => void) {
+        return super.once(eventName, listener);
+    }
+}
