ecccc333ae3e94771889be4941680c6c1a997cac
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-14 15:42:26 +0300

    Add IPC method for re-fetching the Vite server config.

--- npm-packages/meteor-vite/src/bin/worker/vite-server.ts
+++ npm-packages/meteor-vite/src/bin/worker/vite-server.ts
@@ -1,11 +1,12 @@
 import Path from 'path';
 import { createServer, resolveConfig, ViteDevServer } from 'vite';
+import Logger from '../../Logger';
 import { MeteorViteConfig } from '../../vite/MeteorViteConfig';
 import { MeteorStubs } from '../../vite';
 import { ProjectJson } from '../../vite/plugin/MeteorStubs';
 import CreateIPCInterface, { IPCReply } from './IPC/interface';
 
-let server: ViteDevServer;
+let server: ViteDevServer & { config: MeteorViteConfig };
 let viteConfig: MeteorViteConfig;
 
 type Replies = IPCReply<{
@@ -18,19 +19,12 @@ type Replies = IPCReply<{
 }>
 
 export default CreateIPCInterface({
+    async 'vite.getDevServerConfig'(replyInterface: Replies) {
+        sendViteConfig(replyInterface);
+    },
+    
     // todo: Add reply for triggering a server restart
-    async 'vite.startDevServer'(reply: Replies, { packageJson }: { packageJson: ProjectJson }) {
-        
-        const sendViteConfig = (config: MeteorViteConfig) => {
-            reply({
-                kind: 'viteConfig',
-                data: {
-                    host: config.server?.host,
-                    port: config.server?.port,
-                    entryFile: config.meteor?.clientEntry,
-                }
-            })
-        }
+    async 'vite.startDevServer'(replyInterface: Replies, { packageJson }: { packageJson: ProjectJson }) {
         
         viteConfig = await resolveConfig({
             configFile: packageJson?.meteor?.viteConfig,
@@ -52,7 +46,7 @@ export default CreateIPCInterface({
                         name: 'meteor-handle-restart',
                         buildStart () {
                             if (!listening) {
-                                sendViteConfig(server.config);
+                                sendViteConfig(replyInterface);
                             }
                         },
                     },
@@ -62,7 +56,25 @@ export default CreateIPCInterface({
         
         let listening = false
         await server.listen()
-        sendViteConfig(server.config);
+        sendViteConfig(replyInterface);
         listening = true
     }
 })
+
+function sendViteConfig(reply: Replies) {
+    if (!server) {
+        Logger.debug('Tried to get config from Vite server before it has been created!');
+        return;
+    }
+    
+    const { config } = server;
+    
+    reply({
+        kind: 'viteConfig',
+        data: {
+            host: config.server?.host,
+            port: config.server?.port,
+            entryFile: config.meteor?.clientEntry,
+        }
+    })
+}
