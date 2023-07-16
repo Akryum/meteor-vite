b59049c555678fcff271cc601b7dbc5e2aabf93e
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-16 13:16:03 +0300

    Update auto-imports for Vue example

--- examples/vue/client/main.ts
+++ examples/vue/client/main.ts
@@ -1,5 +1,5 @@
 /**
- * These modules are automatically imported by vite:bundler.
+ * These modules are automatically imported by jorgenvatle:vite-bundler.
  * You can commit these to your project or move them elsewhere if you'd like,
  * but they must be imported somewhere in your Meteor entrypoint file.
  *
@@ -7,14 +7,9 @@
 **/
 import 'meteor/test:lazy';
 import 'meteor/test:ts-modules/re-exports-index';
-import 'meteor/test:ts-modules/relative-module';
 import 'meteor/test:ts-modules/export-star-from';
-import 'meteor/test:ts-modules/subdirectory/module-in-subdirectory';
-import 'meteor/test:modules/other';
-import 'meteor/test:ts-modules/re-exports-source';
-import 'meteor/test:modules/sub-other';
 import 'meteor/test:stub-validation';
-/** End of vite:bundler auto-imports **/
+/** End of vite-bundler auto-imports **/
 
 import { Cookies } from 'meteor/ostrio:cookies'
 
