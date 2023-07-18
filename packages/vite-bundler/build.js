cf94c0c0ef0d406536bb30c96185b3b38a1aa2c6
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-18 12:12:03 +0300

    Fix environment variable check during build setup

--- packages/vite-bundler/build.js
+++ packages/vite-bundler/build.js
@@ -9,7 +9,7 @@ import os from 'node:os';
 if (process.env.NODE_ENV !== 'production') return
 
 // Not in a project (publishing the package)
-if (!process.env.VITE_METEOR_DISABLED) return
+if (process.env.VITE_METEOR_DISABLED) return
 
 const pkg = getProjectPackageJson();
 const meteorMainModule = pkg.meteor?.mainModule?.client
