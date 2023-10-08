b858aa4cb21ce028e879e6c1713f4258e4933900
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-10-08 22:31:45 +0200

    test: fix missing it import

--- npm-packages/meteor-vite/test/MeteorPackage.test.ts
+++ npm-packages/meteor-vite/test/MeteorPackage.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import MeteorPackage from '../src/meteor/package/MeteorPackage'
 import { TsModules } from './__mocks'
 
