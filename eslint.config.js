6c032e2b11c37bb78c6af564913edc38eccaec54
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-10-08 22:25:56 +0200

    chore: eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ const antfu = require('@antfu/eslint-config').default
 module.exports = antfu({
   ignores: [
     'examples/output/',
-    '.meteor',
-    '.npm',
+    '**/.meteor',
+    '**/.npm',
   ],
 })
