e3998c7486255de34e638edbb907f16c01598a68
Jørgen Vatle <jorgen@wcaservices.net>
2023-10-20 01:38:49 +0300

    Add Vite plugin to import React from Meteor bundle.
    Rewrites pre-bundling output by Vite to point to Meteor's React bundle instead.
    
    This is primarily as a proof of concept for future reference. There may be a better way to approach the issue.
    
    Ref: #33

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -1,8 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+/**
+ * @return {import('vite').Plugin}
+ */
+function importReactFromMeteorBundle() {
+  let reactChunk = '';
+  return {
+    name: 'output-gen',
+    enforce: 'pre',
+    resolveId(id, importer, options) {
+      if (!importer.includes('/react.js?')) {
+        return;
+      }
+      reactChunk = id.replace(/^.\//, '');
+      console.log({ id, importer, options, reactChunk });
+    },
+    transform(code, id) {
+      if (!reactChunk) {
+        return;
+      }
+      if (!id.includes(`.vite/deps/${reactChunk}`)) {
+        return;
+      }
+      console.log({ transformId: id, reactChunk });
+      return `
+      var __getOwnPropNames = Object.getOwnPropertyNames;
+      var __commonJS = (cb, mod) => function __require() {
+        return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+      };
+      var require_react = () => require('react');
+      export {
+        __commonJS,
+        require_react,
+      }`
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      importReactFromMeteorBundle(),
+  ],
 
   meteor: {
     clientEntry: "imports/vite-entrypoint.jsx",
